647e38f714689aae463f3cf22b8f7ebe64f49af5
aadityaagrawal <adityakuagrawal@gmail.com>
2023-03-14 21:29:25 +0530

    Child Class added

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,18 @@
 import ListGroup from "./Components/ListGroup";
-
+import Alert from "./Components/Alert";
 
 
 function App() {
   let Elements = ["Mumbai", "Delhi", "Goa", "Pune", "Banglore"];
+  const handleSelect = (item:string)=>{
+    console.log(item);
+  }
   return (
     <div>
-      <ListGroup item={Elements} heading="City "/>
+      <ListGroup item={Elements} heading="City" onSelectedIndex = {handleSelect}/>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
-interface Props{
-    item : string [],
-    heading : string
-
+interface Props {
+  item: string[];
+  heading: string;
+  onSelectedIndex: (item: string) => void;
 }
 
-function ListGroup({item, heading}: Props) {
-  
-  
-//   useState function will automatically update the state of selectedIndex under the hood
+function ListGroup({ item, heading , onSelectedIndex}: Props) {
+  //   useState function will automatically update the state of selectedIndex under the hood
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
@@ -17,14 +15,16 @@ function ListGroup({item, heading}: Props) {
       {item.length === 0 && <p> No item Present </p>}
       <ul className="list-group">
         {item.map((element, index) => (
-          <li   
+          <li
             className={
               index === selectedIndex
                 ? "list-group-item active"
                 : "list-group-item"
             }
             key={element}
-            onClick={()=> setSelectedIndex(index)   }
+            onClick={() => {setSelectedIndex(index);
+            onSelectedIndex(element);
+            }}
           >
             {element}
           </li>
